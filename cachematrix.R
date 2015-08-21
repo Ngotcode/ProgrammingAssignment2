55e56088efe12b18fd4157558ae8d7911c800263
Ngotcode <ngot@janelia.hhmi.org>
2015-08-20 23:17:22 -0400

    cache update

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,9 @@
 ## Matrix inversion is usually a costly computation and there may be some benefit to 
 ##  caching the inverse of a matrix rather than compute it repeatedly
 ##  Assignment: is to write a pair of functions that cache the inverse of a matrix.
+## Computing the inverse of a square matrix can be done with the solve function in R. For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+
+##    For this assignment, assume that the matrix supplied is always invertible.
 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
